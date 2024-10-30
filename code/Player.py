a5e399f2fea0344f0f9d78acb960425cedf954b5
Sales <salescampelodev@gmail.com>
2024-10-30 12:45:25 -0300

    creating main loop, connecting menu and game classes

---
+++ code/Player.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entity import Entity
+
+class Player(Entity):
+    def __int__(self):
+        pass
+    def update(self,):
+        pass
+    def move(self, ):
+        pass
